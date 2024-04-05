90e43679ccc296431b6e75dcf3dd95171a669757
Simon Andrews <simon.andrews@babraham.ac.uk>
2024-04-05 13:45:55 +0100

    added new temporary files for AVITI processing

---
+++ create_external_run_folder_structure_1_lane.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+mkdir Aligned
+mkdir Aligned/Project_External
+mkdir Aligned/Project_External/Sample_lane1
+
+mkdir Unaligned
+mkdir Unaligned/Project_External
+mkdir Unaligned/Project_External/Sample_lane1
