86e22c73694f1122dab545f29abda13d6bfe2cd9
laurabiggins <laurabiggins10@gmail.com>
2025-08-20 13:32:33 +0100

    Updated the process aviti script

--- process_aviti.py
+++ process_aviti.py
@@ -1,17 +1,32 @@
 #!/bin/python3
 
-import subprocess, sys, os
+import subprocess
+import os
 import mysql.connector
+import argparse
+from argparse import RawTextHelpFormatter
 
 # currently needs to be run from /data/AV240405
 # we might move to running it from /data when we make it compatible with MiSeq as well.
 # nohup ~/illuminaprocessing/process_aviti.py [run_folder] > xx.log &
 
+# barcode checking only works for lane1 at the moment
+
 n_fastq_lines = 4000000 # 1 million sequences
 
-def main():
+parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter, description = '''For initial processing of AVITI data - this runs bases2fastq, 
+copies fastq files to /primary, checks the first million barcodes and runs an R script to create a barcode plot.''')
+parser.add_argument('run_folder', type=str, default="", help='run folder name')
+parser.add_argument('--ignore_R2', default=False, action='store_true', help='To only use R1 cycles. Default [False]')
+parser.add_argument('--split_lanes', default=False, action='store_true', help='If lane1 and lane2 need to be kept separate. Cannot currently be used with --ignore_R2. Default [False]')
 
-    run_folder = sys.argv[1]
+args=parser.parse_args()
+
+run_folder = args.run_folder
+ignore_R2 = args.ignore_R2
+split_lanes = args.split_lanes
+
+def main():
 
     run_bases2fastq(run_folder)
 
@@ -29,12 +44,12 @@ def main():
     barcode1_count = get_expected_barcodes(run_folder)
     n_bars_to_check = str(barcode1_count+10)
 
-    get_barcodes_I1(run_folder, n_bars_to_check)
+    get_barcodes_I1(run_folder)
 
     I2_file = f"/primary/{run_folder}/Unaligned/Project_External/Sample_lane1/lane1_NoIndex_L001_I2.fastq.gz"
     if os.path.exists(I2_file):
         dual_coded = True
-        get_barcodes_I2(run_folder, n_bars_to_check)
+        get_barcodes_I2(run_folder)
         sort_top_barcodes(run_folder, n_bars_to_check, dual_coded)
     else:
         print("Single indexed library")
@@ -49,7 +64,7 @@ def main():
         print(f"\n !! Couldn't run barcode plot script barcode_ggplot.R on {run_folder} !!")
         print(err)
 
-    print(f"\nAll done. \nCheck barcode plot before running the barcode splitting script.\n")
+    print("\nAll done. \nCheck barcode plot before running the barcode splitting script.\n")
 
 
 #--------------------------------
@@ -70,7 +85,14 @@ def run_bases2fastq(run_folder):
             print(f"\n !! Couldn't find run folder {run_folder}. Valid run folder required !! \n    Exiting...\n")
             exit()
 
-        bases2fastq_cmd = f"bases2fastq -p 16 --run-manifest ~/illuminaprocessing/aviti_run_manifest.csv {run_folder} {run_folder}/Unaligned"
+        if split_lanes:
+            bases2fastq_cmd = f"bases2fastq -p 16 --split-lanes --run-manifest ~/illuminaprocessing/aviti_run_manifest.csv {run_folder} {run_folder}/Unaligned"
+        elif ignore_R2:
+            # do we really need --r2-cycles and a custom manifest?
+            bases2fastq_cmd = f"bases2fastq -p 16 --r2-cycles 0 --run-manifest ~/illuminaprocessing/aviti_run_manifest_exclude_R2.csv {run_folder} {run_folder}/Unaligned"
+        else: 
+            bases2fastq_cmd = f"bases2fastq -p 16 --run-manifest ~/illuminaprocessing/aviti_run_manifest.csv {run_folder} {run_folder}/Unaligned"
+        
         print(bases2fastq_cmd)
         subprocess.run(bases2fastq_cmd, shell=True, executable="/bin/bash")
         
@@ -85,8 +107,17 @@ def run_bases2fastq(run_folder):
 def rename_fastqs(run_folder):
     try:
         os.chdir(run_folder) 
-        rename_cmd = "rename DefaultSample_ lane1_NoIndex_L001_ Unaligned/Samples/DefaultProject/DefaultSample/*fastq.gz"
-        subprocess.run(rename_cmd, shell=True, executable="/bin/bash")
+
+        if split_lanes:
+            rename_cmd1 = "rename DefaultSample_ lane1_NoIndex_ Unaligned/Samples/DefaultProject/DefaultSample/*L001*fastq.gz"        
+            subprocess.run(rename_cmd1, shell=True, executable="/bin/bash")
+            rename_cmd2 = "rename DefaultSample_ lane2_NoIndex_ Unaligned/Samples/DefaultProject/DefaultSample/*L002*fastq.gz"
+            subprocess.run(rename_cmd2, shell=True, executable="/bin/bash")
+            rename_cmd3 = "rename _001.fastq .fastq Unaligned/Samples/DefaultProject/DefaultSample/*fastq.gz"	
+            subprocess.run(rename_cmd3, shell=True, executable="/bin/bash")
+        else:
+            rename_cmd = "rename DefaultSample_ lane1_NoIndex_L001_ Unaligned/Samples/DefaultProject/DefaultSample/*fastq.gz"
+            subprocess.run(rename_cmd, shell=True, executable="/bin/bash")
 
     except Exception as err:
         print(f"\n !! Couldn't move to {run_folder}. Valid run_folder required !!")
@@ -107,10 +138,14 @@ def create_dirs_primary(run_folder):
             mkdir_cmd = f"mkdir {run_folder}"
             subprocess.run(mkdir_cmd, shell=True, executable="/bin/bash")
             os.chdir(run_folder)
-            subprocess.call("/home/sbsuser/illuminaprocessing/create_external_run_folder_structure_1_lane.sh")
+
+            if split_lanes:
+                subprocess.call("/home/sbsuser/illuminaprocessing/create_external_run_folder_structure_2_lanes.sh")
+            else:
+                subprocess.call("/home/sbsuser/illuminaprocessing/create_external_run_folder_structure_1_lane.sh")
 
     except Exception as err:
-        print(f"\n !! Couldn't create file structure on /primary !!")
+        print("\n !! Couldn't create file structure on /primary !!")
         print(err)
         exit()
 
@@ -122,9 +157,14 @@ def cp_to_primary(run_folder):
 
     try:
         os.chdir(f"/primary/{run_folder}")
-        cp_cmd = f"cp /data/AV240405/{run_folder}/Unaligned/Samples/DefaultProject/DefaultSample/*fastq.gz Unaligned/Project_External/Sample_lane1/ > copy.log"
+        cp_cmd = f"cp /data/AV240405/{run_folder}/Unaligned/Samples/DefaultProject/DefaultSample/*L001*fastq.gz Unaligned/Project_External/Sample_lane1/ > copy.log"
         print(f"\nCopying fastq files from {run_folder} to /primary. This may take a while.........\n") 
         subprocess.run(cp_cmd, shell=True, executable="/bin/bash")
+
+        if split_lanes:
+            cp_cmd2 = f"cp /data/AV240405/{run_folder}/Unaligned/Samples/DefaultProject/DefaultSample/*L002*fastq.gz Unaligned/Project_External/Sample_lane2/ >> copy.log"
+            subprocess.run(cp_cmd2, shell=True, executable="/bin/bash")
+
     except Exception as err:
         print(f"\n !! Couldn't copy fastqs from /data to /primary{run_folder} !!")
         print(err)
@@ -134,7 +174,7 @@ def cp_to_primary(run_folder):
 #---------------------
 # quick barcode check
 #---------------------
-def get_barcodes_I1(run_folder, n_bars_to_check):
+def get_barcodes_I1(run_folder):
 
     try:
         os.chdir(f"/primary/{run_folder}/Unaligned/Project_External/Sample_lane1/")
@@ -144,18 +184,18 @@ def get_barcodes_I1(run_folder, n_bars_to_check):
             subprocess.run(bc_check_cmd, shell=True, executable="/bin/bash")
             
         except Exception as err:
-            print(f"\n !! Couldn't run barcode checking command !!")
+            print("\n !! Couldn't run barcode checking command !!")
             print(err)
             
     except Exception as err:
-            print(f"\n !! Couldn't run barcode checking command !!")
+            print("\n !! Couldn't run barcode checking command !!")
             print(err)
 
 
 #---------------------
 # quick barcode check
 #---------------------
-def get_barcodes_I2(run_folder, n_bars_to_check):
+def get_barcodes_I2(run_folder):
 
     try:
         os.chdir(f"/primary/{run_folder}/Unaligned/Project_External/Sample_lane1/")
@@ -165,11 +205,11 @@ def get_barcodes_I2(run_folder, n_bars_to_check):
             subprocess.run(bc_check_cmd, shell=True, executable="/bin/bash")
             
         except Exception as err:
-            print(f"\n !! Couldn't run barcode checking command !!")
+            print("\n !! Couldn't run barcode checking command !!")
             print(err)
             
     except Exception as err:
-        print(f"\n !! Couldn't run barcode checking command !!")
+        print("\n !! Couldn't run barcode checking command !!")
         print(err)
 
 #---------------------
@@ -189,11 +229,11 @@ def sort_top_barcodes(run_folder, n_bars_to_check, dual_coded):
             subprocess.run(bc_sort_cmd, shell=True, executable="/bin/bash")
             
         except Exception as err:
-            print(f"\n !! Couldn't run barcode sorting command !!")
+            print("\n !! Couldn't run barcode sorting command !!")
             print(err)
             
     except Exception as err:
-        print(f"\n !! Couldn't run barcode sorting command !!")
+        print("\n !! Couldn't run barcode sorting command !!")
         print(err)
 
 # This writes out the expected barcodes to a text file and returns the number of expected barcodes.
@@ -225,7 +265,7 @@ def get_expected_barcodes(run_folder):
         return(barcode1_count)
         
     except Exception as err:
-        print(f"\n !! Couldn't get expected barcodes !!")
+        print("\n !! Couldn't get expected barcodes !!")
         print(err)
 
 
