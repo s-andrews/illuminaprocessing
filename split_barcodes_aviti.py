73b935919c2662817693ee40059442b19232ef2f
s-andrews <simon@proteo.me.uk>
2025-09-29 14:12:28 +0100

    Close Popens properly

--- split_barcodes_aviti.py
+++ split_barcodes_aviti.py
@@ -305,9 +305,9 @@ def open_filehandlesR2(fname, sample_level_barcode, path_from_run_folder):
 
 def close_filehandles():
 	for name in fhsR1.keys():
-		fhsR1[name].close() 
+		fhsR1[name].stdout.close() 
 	for name in fhsR2.keys():
-		fhsR2[name].close() 
+		fhsR2[name].stdout.close() 
 
 
 def reverse_complement(dna_seq):
